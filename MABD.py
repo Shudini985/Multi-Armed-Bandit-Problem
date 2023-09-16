8b982fc5649b3ec72bb2b8c9a8279def1ccfa64e
Shuhood Syed <141329865+Shudini985@users.noreply.github.com>
2023-09-16 12:32:58 +0300

    Add files via upload

---
+++ MABD.py
@@ -0,0 +1,40 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_Optimisation.csv')
+
+# Implementing UCB
+import math
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_reward = [0] * d
+total_reward = 0
+
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if (numbers_of_selections[i] > 0):
+            average_reward = sums_of_reward[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(2 * math.log(n+1) / numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+    ads_selected.append(ad)
+    numbers_of_selections[ad] += 1
+    reward = dataset.values[n, ad]
+    sums_of_reward[ad] += reward
+    total_reward += reward
+
+# Calculating ad probability
+ad_probabilities = [numbers_of_selections[i] / N for i in range(d)]
+for i in range(d):
+    print(f"Ad {i + 1}: Probability = {ad_probabilities[i]:.4f}")
